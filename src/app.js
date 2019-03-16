375c727315607c8d80b5d4a965e20ee6c202bb68
halfrice <halfrice@pm.me>
2019-03-16 15:52:33 -0700

    Parcel

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ])
-}
+import React from "react"
+import ReactDOM from "react-dom"
+
+import Pet from "./pet"
 
 class App extends React.Component {
   render() {
